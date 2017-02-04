6ff53a404fb1e7d7c207d66b69ee70b6ecb6319a
EQuimper <quimperemanuel@gmail.com>
2017-02-04 17:04:41 -0500

    Add prod webpack

--- src/modules/posts/Posts.js
+++ src/modules/posts/Posts.js
@@ -23,6 +23,10 @@ class Posts extends Component {
       return (
         <h1>Loading...</h1>
       );
+    } else if (this.props.posts.length === 0) {
+      return (
+        <h1>No post yet</h1>
+      )
     }
     return (
       <div>
---
+++ webpack.prod.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const { join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'styled-components', 'react-redux', 'redux'
+];
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    bundle: [
+      'babel-polyfill',
+      './src/index.js'
+    ],
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+};
