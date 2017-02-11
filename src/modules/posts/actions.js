faddfcc56153d9546389498086d27766673ebfcf
EQuimper <quimperemanuel@gmail.com>
2017-02-11 14:27:07 -0500

    Error handler in single post

--- src/modules/posts/actions.js
+++ src/modules/posts/actions.js
@@ -21,14 +21,22 @@ export const getFetchAllPosts = () => async dispatch => {
  * FETCH SINGLE POST WITH HIS ID
  */
 export const FETCH_SINGLE_POST = 'FETCH_SINGLE_POST';
+export const FETCH_SINGLE_POST_ERROR = 'FETCH_SINGLE_POST_ERROR';
 
 const fetchPost = post => ({
   type: FETCH_SINGLE_POST,
   post
 });
 
+const fetchPostError = () => ({
+  type: FETCH_SINGLE_POST_ERROR
+});
+
 export const getFetchSinglePost = id => async dispatch => {
   const { post } = await postApi.fetchSinglePost(id);
+  if (!post) {
+    return dispatch(fetchPostError());
+  }
   return dispatch(fetchPost(post));
 };
 
--- src/modules/posts/reducer.js
+++ src/modules/posts/reducer.js
@@ -1,13 +1,15 @@
 import {
   FETCH_ALL_POSTS,
   FETCH_SINGLE_POST,
+  FETCH_SINGLE_POST_ERROR,
   SELECTED_POST
 } from './actions';
 
 const INITIAL_STATE = {
   posts: [],
   post: null,
-  selectPostId: null
+  selectPostId: null,
+  error: false
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -15,16 +17,25 @@ export default (state = INITIAL_STATE, action) => {
     case FETCH_ALL_POSTS:
       return {
         ...state,
+        error: false,
         posts: action.posts
       };
     case FETCH_SINGLE_POST:
       return {
         ...state,
-        post: action.post
+        error: false,
+        post: action.post,
+
+      };
+    case FETCH_SINGLE_POST_ERROR:
+      return {
+        ...state,
+        error: true
       };
     case SELECTED_POST:
       return {
         ...state,
+        error: false,
         selectPostId: action.id
       };
     default:
