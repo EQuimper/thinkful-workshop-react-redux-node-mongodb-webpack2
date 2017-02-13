67341e08cef4492f2e36705b558fb1e3a4da8346
EQuimper <quimperemanuel@gmail.com>
2017-02-13 15:24:02 -0500

    Add type to controller

--- server/modules/posts/controller.js
+++ server/modules/posts/controller.js
@@ -1,24 +1,29 @@
+/** @flow */
 import Post from './model';
 
 /**
 * Create
 */
-export const createPost = async (req, res) => {
-  const { title, text } = req.body;
+export const createPost = async (req: Object, res: Object) => {
+  const { title, text } : { title: string, text: string } = req.body;
 
   if (!title) {
     return res.status(401).json({ error: true, message: 'Title required' });
+  } else if (typeof title !== 'string') {
+    return res.status(401).json({ error: true, message: 'Title must be a string' });
   } else if (title.length < 6) {
     return res.status(401).json({ error: true, message: 'Title must be 5 characters long' });
   }
 
   if (!text) {
     return res.status(401).json({ error: true, message: 'Text required' });
+  } else if (typeof text !== 'string') {
+    return res.status(401).json({ error: true, message: 'Text must be a string' });
   } else if (text.length < 31) {
     return res.status(401).json({ error: true, message: 'Text must be 30 characters long' });
   }
 
-  const newPost = new Post({ title, text });
+  const newPost: Post = new Post({ title, text });
 
   try {
     return res.status(200).json({ error: false, post: await newPost.save() });
@@ -30,7 +35,7 @@ export const createPost = async (req, res) => {
 /**
 * GET ALL
 */
-export const fetchPosts = async (req, res) => {
+export const fetchPosts = async (req: Object, res: Object) => {
   try {
     return res.status(200).json({ error: false, posts: await Post.find({}) });
   } catch (e) {
@@ -41,7 +46,7 @@ export const fetchPosts = async (req, res) => {
 /**
 * GET BY ID
 */
-export const fetchPostById = async (req, res) => {
+export const fetchPostById = async (req: Object, res: Object) => {
   try {
     return res.status(200).json({ error: false, post: await Post.findById(req.params.id) });
   } catch (e) {
