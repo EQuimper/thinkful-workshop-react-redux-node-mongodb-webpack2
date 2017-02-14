09c77084cf86ea18da645893977ed622bd6327b6
EQuimper <quimperemanuel@gmail.com>
2017-02-14 18:00:27 -0500

    Function error

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,13 @@ import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import App from './layout/App';
 
+const errorLoading = (err: Object) =>
+ console.error('Dynamic page loading failed', err);
+
+
+const loadRoute = (cb: Function) =>
+  (module: Object) => cb(null, module.default);
+
 const componentRoutes = {
   component: App,
   path: '/',
@@ -11,14 +18,16 @@ const componentRoutes = {
       path: '/posts',
       getComponent(location: string, cb) {
         System.import('./modules/posts/Posts')
-          .then(module => cb(null, module.default));
+          .then(loadRoute(cb))
+          .catch(errorLoading);
       }
     },
     {
       path: '/posts/:id',
       getComponent(location: string, cb) {
         System.import('./modules/posts/SinglePost')
-          .then(module => cb(null, module.default));
+          .then(loadRoute(cb))
+          .catch(errorLoading);
       }
     }
   ]
