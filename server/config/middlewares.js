ac97df8de62ec60294a7260c36ceaa78f1bb0460
EQuimper <quimperemanuel@gmail.com>
2017-02-13 19:12:58 -0500

    Add compression

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,7 +1,9 @@
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import compression from 'compression';
 
 export default app => {
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan('dev'));
