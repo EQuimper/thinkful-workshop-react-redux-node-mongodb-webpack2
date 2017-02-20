a7053ede4d24a54b01642c745c4c7550b8abad49
EQuimper <quimperemanuel@gmail.com>
2017-02-19 20:30:18 -0500

    Add new styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,16 @@
-/** @flow */
 import styled from 'styled-components';
 
 const Button = styled.div`
+  background-color: #6799FF;
   height: 40px;
-  width: 125px;
-  background-color: #ec3c3c;
-  border-radius: 5px;
-  cursor: pointer;
+  width: 100px;
+  border-radius: 10px;
+  color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
   display: flex;
   justify-content: center;
   align-items: center;
-
-  &:hover {
-    box-shadow: 1px 1px 1px black;
-  }
+  cursor: pointer;
 `;
 
 export default Button;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const Card = styled.div`
-  height: 400px;
-  width: 300px;
+  height: 25em;
+  width: 25em;
   background-color: #fff;
-  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
 `;
 
 export default Card;
