386f84feedb632a3e1af9fd0e1a4a30cd1181f71
EQuimper <quimperemanuel@gmail.com>
2017-02-22 12:54:29 -0500

    Add prod var

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -4,8 +4,13 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import compression from 'compression';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default (app: $Application) => {
-  app.use(compression());
+  if (isProd) {
+    console.log('hello');
+    app.use(compression());
+  }
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan('dev'));
