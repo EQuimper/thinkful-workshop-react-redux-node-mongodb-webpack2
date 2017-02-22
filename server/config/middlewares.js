a75a4472f00c0e8b73a29c524dee9cc1ef50f84f
EQuimper <quimperemanuel@gmail.com>
2017-02-22 12:57:30 -0500

    Add comment

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -8,9 +8,14 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default (app: $Application) => {
   if (isProd) {
+    // Make a gzip with the server
+    // Better perf with that
+    // Only use in production
     app.use(compression());
   }
+  // This is for parse the req.body to a json format
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  // We use morgan here for get the request in the console
   app.use(morgan('dev'));
 };
