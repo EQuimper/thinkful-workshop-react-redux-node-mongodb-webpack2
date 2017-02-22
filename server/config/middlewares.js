5a5f9cae517acc159192a6f49a3d0614d0bfe644
EQuimper <quimperemanuel@gmail.com>
2017-02-22 14:36:45 -0500

    Change middlewares

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -3,8 +3,6 @@ import { type $Application } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import compression from 'compression';
-import fs from 'fs';
-import { join } from 'path';
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -14,13 +12,9 @@ export default (app: $Application) => {
     // Better perf with that
     // Only use in production
     app.use(compression());
-    // Create a file with the log from morgan
-    const accessLogStream = fs.createWriteStream(join(__dirname, 'access.log'), { flags: 'a' });
-    app.use(morgan('combined', { stream: accessLogStream }));
-  } else {
-    // We use morgan here for get the request in the console
-    app.use(morgan('dev'));
   }
+  // We use morgan here for get the request in the console
+  app.use(morgan('dev'));
   // This is for parse the req.body to a json format
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
